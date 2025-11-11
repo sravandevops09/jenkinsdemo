f927876305f78064122cc52e02680c9052683605
sravandevops09 <sravankumar.devops09@gmail.com>
2025-11-11 18:32:07 +0530

    Update main.tf

--- deployment/main.tf
+++ deployment/main.tf
@@ -35,16 +35,20 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
+
 
 
 # 🔹 Create an EC2 instance using t3.micro
 resource "aws_instance" "terraform_demo" {
   ami           = data.aws_ami.amazon_linux_2023.id
   instance_type = "t3.micro"
-subnet_id     = element(data.aws_subnet_ids.default.ids, 0)
+subnet_id     = element(data.aws_subnets.default.ids, 0)
 
   tags = {
     Name = "TerraformDemoInstance"
